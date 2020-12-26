17112ec642dc021474360912a5bc1c0f91c9317c
Max <mjann@ucsc.edu>
2020-12-26 14:21:34 -0800

    can't fix

--- nerves.js
+++ nerves.js
@@ -3,9 +3,8 @@ function hoverboard(thing){
         console.log("you are hovering over chess");
         document.getElementById("rapper").className="chess";
         document.getElementById("secret").innerHTML=`
-        <div>
-            <h1>2000+ Chess Rating</h1>
-            <h2>Codejoy on lichess.org</h2>
+        <div class="resize">
+            <a href="https://lichess.org/@/codejoy/perf/classical"><div id="chesslink">I am a chess expert</div></a>
         </div>
         `
 
@@ -14,7 +13,7 @@ function hoverboard(thing){
         console.log("you are hovering over school");
         document.getElementById("rapper").className="school";
         document.getElementById("secret").innerHTML=`
-        <div>
+        <div class="resize">
             <h1>University of California at Santa Cruz</h1>
             <h2>B.A. Philsophy Class of 2017</h2>
 
@@ -25,8 +24,7 @@ function hoverboard(thing){
         console.log("you are hovering over space");
         document.getElementById("rapper").className="space";
         document.getElementById("secret").innerHTML=`
-        <div>
-            <h1>Planet Earth</h1>
+        <div class="resize">
             <h2>Thousand Oaks, California</h2>
         </div>
         `
@@ -35,9 +33,18 @@ function hoverboard(thing){
         console.log("you are hovering over the reset");
         document.getElementById("rapper").className="neutral";
         document.getElementById("secret").innerHTML=`
-        <p>
-            Hover around!
-        </p>
+        <div class="resize">
+            <h2>Hover Around!</h2>
+        </div>
+        `
+    }
+    else if(thing == 'music'){
+        console.log("you are hovering over music");
+        document.getElementById("rapper").className="music";
+        document.getElementById("secret").innerHTML=`
+        <div class="resize">
+        <h2>I am an amateur singer-songwriter</h2>
+        <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/954293368&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/maxwelljann" title="Maxwell Jann" target="_blank" style="color: #cccccc; text-decoration: none;">Maxwell Jann</a> · <a href="https://soundcloud.com/maxwelljann/riding-too-high" title="Riding Too High" target="_blank" style="color: #cccccc; text-decoration: none;">Riding Too High</a></div>
         `
     }
     else{
